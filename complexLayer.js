9b75e6201637458a6984a6c30ff9bd6c1126fdaa
ItamarTati <itamar.tati1994@gmail.com>
2023-09-25 21:47:46 +0100

    Added more logic

---
+++ complexLayer.js
@@ -0,0 +1,106 @@
+//                         ╔══════════════════════════════════╗
+//                         ║                                  ║
+//                         ║   Don't Change any Code Here     ║
+//                         ║               Please.            ║
+//                         ║                                  ║
+//                         ╚══════════════════════════════════╝
+//
+
+export const numbersArray = [1, 2, 3, 4, 5];
+
+export const stringsArray = ["Apple", "Banana", "Cherry", "Date", "Fig"];
+
+export const booleansArray = [true, false, true, false, true];
+
+export const heroData =  [
+    {
+        name: "Izuku Midoriya (Deku)",
+        description: "The main protagonist and a student at U.A. High School with a powerful Quirk inherited from All Might.",
+        image: "image_url_for_deku.jpg",
+        quirk: "One For All"
+    },
+    {
+        name: "Katsuki Bakugo (Kacchan)",
+        description: "Another student at U.A. High School and a childhood friend/rival of Izuku. His Quirk allows him to create explosions.",
+        image: "image_url_for_bakugo.jpg",
+        quirk: "Explosion"
+    },
+    {
+        name: "All Might",
+        description: "The Symbol of Peace and the former No. 1 Hero with the Quirk 'One For All.' He mentors Izuku.",
+        image: "image_url_for_all_might.jpg",
+        quirk: "One For All"
+    },
+    {
+        name: "Shoto Todoroki",
+        description: "A student at U.A. High School with both ice and fire powers due to his parentage.",
+        image: "image_url_for_todoroki.jpg",
+        quirk: "Half-Cold Half-Hot"
+    },
+    {
+        name: "Ochaco Uraraka (Uravity)",
+        description: "A cheerful U.A. student who can manipulate gravity with her Quirk.",
+        image: "image_url_for_uraraka.jpg",
+        quirk: "Zero Gravity"
+    },
+    {
+        name: "Tenya Iida (Ingenium)",
+        description: "Known for his incredible speed, he's a classmate of Izuku and Ochaco.",
+        image: "image_url_for_iida.jpg",
+        quirk: "Engine"
+    },
+    {
+        name: "Tsuyu Asui (Froppy)",
+        description: "A U.A. student with frog-like abilities, including a long, extendable tongue.",
+        image: "image_url_for_asui.jpg",
+        quirk: "Frog"
+    },
+    {
+        name: "Eijiro Kirishima (Red Riot)",
+        description: "Known for his hardening Quirk, making his body nearly indestructible.",
+        image: "image_url_for_kirishima.jpg",
+        quirk: "Hardening"
+    },
+    {
+        name: "Momo Yaoyorozu (Creati)",
+        description: "A highly intelligent student at U.A. who can create objects from her body.",
+        image: "image_url_for_yaoyorozu.jpg",
+        quirk: "Creation"
+    },
+    {
+        name: "Dabi",
+        description: "A villain with fire-based powers and a member of the League of Villains.",
+        image: "image_url_for_dabi.jpg",
+        quirk: "Cremation"
+    },
+    {
+        name: "Hawks",
+        description: "The No. 2 Hero known for his mastery of flight and control over feathers.",
+        image: "image_url_for_hawks.jpg",
+        quirk: "Fierce Wings"
+    },
+    {
+        name: "Mirko",
+        description: "A pro hero with super-speed and rabbit-like attributes.",
+        image: "image_url_for_mirko.jpg",
+        quirk: "Rabbit"
+    },
+    {
+        name: "Nejire Hado",
+        description: "A member of the 'Big Three' at U.A. High School with energy-based powers.",
+        image: "image_url_for_nejire.jpg",
+        quirk: "Wave Motion"
+    },
+    {
+        name: "Tamaki Amajiki (Suneater)",
+        description: "Another member of the 'Big Three' who can transform parts of his body into food.",
+        image: "image_url_for_amajiki.jpg",
+        quirk: "Manifest"
+    },
+    {
+        name: "Chizome Akaguro (Stain)",
+        description: "A vigilante with the Quirk 'Bloodcurdle' that allows him to paralyze his victims by licking their blood.",
+        image: "image_url_for_stain.jpg",
+        quirk: "Bloodcurdle"
+    }
+]
